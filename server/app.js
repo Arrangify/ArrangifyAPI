ac34d553e615520a481bc604d49f72c98a25327f
neonboxx <neonboxx@gmail.com>
2016-03-19 19:06:01 +0000

    added auth

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('express');
 var mongoose = require('mongoose');
 var config = require('./config/environment');
+var jwt = require('express-jwt');
 
 // Connect to database
 mongoose.connect(config.mongo.uri, config.mongo.options);
@@ -24,6 +25,14 @@ if(config.seedDB) { require('./config/seed'); }
 // Setup server
 var app = express();
 var server = require('http').createServer(app);
+
+var jwtCheck = jwt({
+  secret: new Buffer('f4AGFyYmmTJdzDMg4b4P6bwWYgacKvXI-5pbToxH0o_aSTI2qtEJ3Gx7ky1W6Qq4', 'base64'),
+  audience: 'aTXYBsgnA1yrAUygOY138OilcGObKtCV'
+});
+
+app.use('/api/users', jwtCheck);
+
 require('./config/express')(app);
 require('./routes')(app);
 
