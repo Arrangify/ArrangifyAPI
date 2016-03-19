21724d71a9f708e6d607a1be42a79a248340e1e7
neonboxx <neonboxx@gmail.com>
2016-03-19 15:57:45 +0000

    fix travis file

--- routes/events.js
+++ routes/events.js
@@ -3,9 +3,19 @@ var router = express.Router();
 
 router.get('/', function(req, res) {
     var db = req.db;
-    var collection = db.get('users');
+    var collection = db.get('events');
     collection.find({},{},function(e,docs){
         res.json(docs);
+    });
+});
+
+router.post('/', function(req, res) {
+    var db = req.db;
+    var collection = db.get('events');
+    collection.insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
     });
 });
 
