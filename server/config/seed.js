4b558ca9a0004ad2d29da978407c40b1e25fbaaf
neonboxx <neonboxx@gmail.com>
2016-03-19 18:21:28 +0000

    removed thing endpoint

--- server/config/seed.js
+++ server/config/seed.js
@@ -6,19 +6,12 @@
 'use strict';
 // Insert seed models below
 var Event = require('../api/event/event.model');
-var Thing = require('../api/thing/thing.model');
 
 
 // Insert seed data below
 var eventSeed = require('../api/event/event.seed.json');
-var thingSeed = require('../api/thing/thing.seed.json');
 
 // Insert seed inserts below
 Event.find({}).remove(function() {
 	Event.create(eventSeed);
-});
-
-
-Thing.find({}).remove(function() {
-  Thing.create(thingSeed);
 });
